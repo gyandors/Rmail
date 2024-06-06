685c58945e2a83fdc6c7c45ce51a5c0b8a8b6d52
Sachin Gyandor <gyandors@gmail.com>
2024-06-07 01:39:14 +0530

    Added test files

---
+++ src/__tests__/SignUp.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+
+import SignUp from '../components/SignUp';
+
+import { BrowserRouter } from 'react-router-dom';
+
+const component = (
+  <BrowserRouter>
+    <SignUp />
+  </BrowserRouter>
+);
+
+describe('Testing SignUp page', () => {
+  test('Should render h1 tag with "SignUp" text', () => {
+    render(component);
+
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+  });
+
+  test('Should render label with "Email" text', () => {
+    render(component);
+
+    expect(screen.getByLabelText('Email')).toBeInTheDocument();
+  });
+});
